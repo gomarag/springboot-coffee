7e2c74fe0d264be6389123b3f474eb477ac107e5
gomarag <maragriver@gmail.com>
2023-06-24 20:29:11 +0900

    04-CRUD 메소드 리팩토링: CRUDRepsitory 내장 메소드로 변경

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -88,42 +88,45 @@ class RestApiDemoController {
 	// }
 	@GetMapping("/")
 	Iterable<Coffee> getCoffees() {
-		return coffees;
+		return coffeeRepository.findAll();
 	}
 
 	@GetMapping("/{id}")
 	Optional<Coffee> getCoffeeById(@PathVariable String id) {
-		for (Coffee coffee : coffees) {
-			if (coffee.getId().equals(id)) {
-				return Optional.of(coffee);
-			}
-		}
-		return Optional.empty();
+		return coffeeRepository.findById(id);
+		// for (Coffee coffee : coffees) {
+		// 	if (coffee.getId().equals(id)) {
+		// 		return Optional.of(coffee);
+		// 	}
+		// }
+		// return Optional.empty();
 	}
 
-	@PostMapping("/") 
+	@PostMapping("/")
 	Coffee postCoffee(@RequestBody Coffee coffee) {
-		coffees.add(coffee);
-		return coffee;
+		// coffees.add(coffee);
+		// return coffee;
+		return coffeeRepository.save(coffee);
 	}
 
 	@PutMapping("/{id}")
 	ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-		int coffeeIndex = -1;
+		// int coffeeIndex = -1;
 
-		for (Coffee c : coffees) {
-			if (c.getId().equals(id)) {
-				coffeeIndex = coffees.indexOf(c);
-				coffees.set(coffeeIndex, coffee);
-			}
-		}
-		return (coffeeIndex == -1)
-				? new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED)
-				: new ResponseEntity<>(coffee, HttpStatus.OK);
+		// for (Coffee c : coffees) {
+		// 	if (c.getId().equals(id)) {
+		// 		coffeeIndex = coffees.indexOf(c);
+		// 		coffees.set(coffeeIndex, coffee);
+		// 	}
+		// }
+		return (!coffeeRepository.existsById(id))
+				? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED)
+				: new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK);
 	}
 
 	@DeleteMapping("/{id}")
 	void deleteCoffee(@PathVariable String id) {
-		coffees.removeIf(coffee -> coffee.getId().equals(id));
+		// coffees.removeIf(coffee -> coffee.getId().equals(id));
+		coffeeRepository.deleteById(id);
 	}
 }
